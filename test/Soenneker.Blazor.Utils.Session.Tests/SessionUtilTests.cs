30b5c6248914149de8844e938cbcc8407f926031
Jake Soenneker <jake@soenneker.com>
2025-09-08 14:18:30 -0500

    fixes test

--- test/Soenneker.Blazor.Utils.Session.Tests/SessionUtilTests.cs
+++ test/Soenneker.Blazor.Utils.Session.Tests/SessionUtilTests.cs
@@ -1,18 +1,13 @@
-using Soenneker.Blazor.Utils.Session.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-
 
 namespace Soenneker.Blazor.Utils.Session.Tests;
 
 [Collection("Collection")]
 public class SessionUtilTests : FixturedUnitTest
 {
-    private readonly ISessionUtil _util;
-
     public SessionUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<ISessionUtil>(true);
     }
 
     [Fact]
